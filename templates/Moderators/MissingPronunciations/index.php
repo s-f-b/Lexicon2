6265ee985b84672b9ea68c268504fa1a936c7882
Shoshana Boardman <shoshanaboardman@gmail.com>
2025-08-14 14:44:19 -0400

    New controller for pronunciations

---
+++ templates/Moderators/MissingPronunciations/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var \Cake\Datasource\ResultSetInterface $noPronunciations
+ * @var object $sitelang
+ */
+?>
+<h2>Missing Pronunciations</h2>
+<p>All approved words have pronunciations.</p>
